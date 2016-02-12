a0ac3f347e7c556efe8a8b135329e4a9a77ebfe6
Slimikus <slimikus@gmail.com>
2016-02-12 15:10:11 +0300

    Создание приложения

---
+++ iDecide/iDecide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iDecide
+//
+//  Created by Admin on 12.02.16.
+//  Copyright © 2016 Slimikus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iDecide/iDecide/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iDecide
+//
+//  Created by Admin on 12.02.16.
+//  Copyright © 2016 Slimikus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
